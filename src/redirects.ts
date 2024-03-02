33db4c7eeadae8244c69063b30f82b6181808080
sexnine <25860388+sexnine@users.noreply.github.com>
2024-03-03 08:22:53 +1100

    Add /xyz

--- src/redirects.ts
+++ src/redirects.ts
@@ -3,6 +3,7 @@ const redirects: Record<string, string> = {
   "/dash": "https://dash.hackpnw.org",
   "/discord": "https://discord.com/invite/4DsTdBEEH4",
   "/hackthesound": "https://www.hackthesound.net/?utm_source=hackpnw&ref=hackpnw",
+  "/xyz": "https://drive.google.com/file/d/1VWbL8W2QnwjZJXGU26X9m1lqSTcMutlE/view",
 };
 
 export default redirects;
