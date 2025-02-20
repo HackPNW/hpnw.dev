c25fb3a288b4c78c77824d04e870cfd00ed2ef80
sexnine <25860388+sexnine@users.noreply.github.com>
2025-02-20 14:42:39 +1100

    add iro redirects

--- src/redirects.ts
+++ src/redirects.ts
@@ -4,6 +4,11 @@ const redirects: Record<string, string> = {
   "/discord": "https://discord.com/invite/4DsTdBEEH4",
   "/hackthesound": "https://www.hackthesound.net/?utm_source=hackpnw&ref=hackpnw",
   "/xyz": "https://drive.google.com/file/d/1VWbL8W2QnwjZJXGU26X9m1lqSTcMutlE/view",
+  "/iro-25":
+    "https://internationalresearcholympiad.org/?utm_source=hackpnw&utm_campaign=hackpnw-email-2025-02&utm_medium=email",
+  "/iro-25-club":
+    "https://www.internationalresearcholympiad.org/start-iro-club?utm_source=hackpnw&utm_campaign=hackpnw-email-2025-02&utm_medium=email",
+  "/iro-25-vid": "https://www.youtube.com/watch?v=pZcqxaMV3v0",
 };
 
 export default redirects;
